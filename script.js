3ce127567c7f864b9b1003dfc0e819df7a880433
adilsaiyad35-cpu <adilsaiyad35@gmail.com>
2026-07-03 10:47:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const images = [
+    "home.jfif",
+    "home2.jfif",
+    "home3.jfif",
+    "home4.jfif"
+];
+
+let current = 0;
+const img = document.getElementById("aboutImage");
+
+setInterval(() => {
+    img.style.opacity = 0;
+
+    setTimeout(() => {
+        current = (current + 1) % images.length;
+        img.src = images[current];
+        img.style.opacity = 1;
+    }, 500);
+
+}, 10000); // 10 seconds
